cb6715c8e3f6c3a7ca507e304d78b097ff7e20f0
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-06-03 22:53:15 -0300

    add links

--- src/Components/Links/index.js
+++ src/Components/Links/index.js
@@ -8,10 +8,19 @@ export default function Links() {
                     <a className="nav-link" aria-current="page" target='_blank' href="https://viniciusduarte.vercel.app/">Meu Portfólio</a>
                 </div>
                 <div className='div-link d-flex flex-column align-items-center justify-content-center'>
+                    <a className="nav-link" aria-current="page" target='_blank' href="https://www.linkedin.com/in/vinicius-franco-duarte/">Linkedin</a>
+                </div>
+                <div className='div-link d-flex flex-column align-items-center justify-content-center'>
                     <a className="nav-link" aria-current="page" target='_blank' href="https://api.whatsapp.com/send?phone=5524999879994&text=Olá, Gostaria de fazer um orçamento!">Whatsapp</a>
                 </div>
                 <div className='div-link d-flex flex-column align-items-center justify-content-center'>
+                    <a className="nav-link" aria-current="page" target='_blank' href="https://github.com/francovinicius">Github</a>
+                </div>
+                <div className='div-link d-flex flex-column align-items-center justify-content-center'>
                     <a className="nav-link" aria-current="page" target='_blank' href="https://www.instagram.com/duaartevini/">Instagram</a>
+                </div>
+                <div className='div-link d-flex flex-column align-items-center justify-content-center'>
+                    <span className="nav-link">(Em breve) - Baixe meu e-book "Como vencer a procrastinação"</span>
                 </div>
             </div>
 
