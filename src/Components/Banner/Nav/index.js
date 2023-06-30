4a5d4f8ccd32532252d1d769c7c0de75dfd40391
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-06-30 11:22:06 -0300

    alt layout

---
+++ src/Components/Banner/Nav/index.js
@@ -0,0 +1,16 @@
+import style from './Nav.css'
+
+export default function Nav() {
+    return (
+        <div className='d-flex align-items-center justify-content-center navegacao'>
+            <a className="tag-a nav-link" href="/about">Sobre</a>
+            <a className="tag-a nav-link" href="/">Links</a>
+        </div>
+    )
+}
+
+
+
+
+
+
--- src/Components/Banner/index.js
+++ src/Components/Banner/index.js
@@ -1,19 +1,17 @@
 import styles from './Banner.css'
+import Nav from './Nav'
 
 export default function Banner() {
     return (
-        <section>
-            <div className="d-flex flex-column align-items-center justify-content-center">
-                <div className='d-flex align-items-center justify-content-center navegacao'>
-                    <a className="nav-link" href="/about">Sobre</a>
-                    <a className="nav-link" href="/">Links</a>
-                </div>
 
+
+        <section>
+            <Nav />
+
+            <div className="d-flex flex-column align-items-center justify-content-center">
                 <img className="img-perfil img-fluid" src="./assets/img/vn.jpeg" alt="Minha foto" />
                 <h3>Vinicius Duarte</h3>
                 <span className='slogan'>Desenvolvedor web</span>
-
-
             </div>
 
         </section>
